1f7d0abc6b7b3d6c37417509bd3cf164adc34ab3
calllumwhhui <huicallum@gmail.com>
2024-11-12 15:49:45 +0000

    Added Fuel tank

--- tank/character.cs
+++ tank/character.cs
@@ -11,12 +11,16 @@ public partial class character : CharacterBody2D
   public const float _angularSpeed = Mathf.Pi;
   public const float _boostMultiplier = 1.5f;
   private bool _boosting = false;
+  public float MaxFuel = 100.0f;
+  public float CurrentFuel {get; set;}
+  public float FuelConsumptionRate = 10f;
 
   private Node2D _child = null;
 
   public override void _Ready()
   {
     _child = GetNode<Node2D>("cannon");
+    CurrentFuel = MaxFuel;
   }
 
   private void _shoot()
@@ -33,6 +37,7 @@ public partial class character : CharacterBody2D
   public override void _PhysicsProcess(double delta)
   {
     Vector2 velocity = Vector2.Zero;
+    
 
     var currentSpeed = Speed;
     var cannonDirection = 0;
@@ -76,6 +81,18 @@ public partial class character : CharacterBody2D
     if (Input.IsActionJustPressed("ui_accept"))
     {
       _shoot();
+    }
+
+    if (velocity.Length() > 0 || _boosting)
+    {
+      CurrentFuel -= FuelConsumptionRate * (float)delta;
+    }
+
+    if (CurrentFuel <= 0)
+    {
+      CurrentFuel = 0;
+      velocity = Vector2.Zero;
+      GD.Print("Fresh Out of Fuel!!");
     }
 
 
